130c9d20396cebf6d3acb83a621bbd061ebc5c50
radha-codes <prabhakarradha428@gmail.com>
2026-07-10 15:16:14 +0530

    Add files via upload

---
+++ ch1_modules_comments.py
@@ -0,0 +1,13 @@
+print("Hello World")
+import pyjokes
+joke = pyjokes.get_joke()
+print(joke)
+#print(Printing Jokes...")
+#This prints a random joke
+joke = pyjokes.get_joke()
+print(joke)
+
+"""
+so thanks 
+that was my program
+"""
