a4efb33e236b180719a79c31ceb08e41d71d4930
radha-codes <prabhakarradha428@gmail.com>
2026-07-11 18:13:18 +0530

    Add files via upload

---
+++ ch2_variables_datatypes.py
@@ -0,0 +1,75 @@
+# Variables
+
+a = 1
+b = 2
+print(a + b )
+ 
+# Datatypes
+
+a = 1 # a is an integer 
+b = 5.22 # b is a floating point number 
+c = "Harry" # c is a string
+d = False # d is a boolean variable
+e = None # e is a none type variable
+
+# Rules for Defining Variables
+a = 23
+aaa = 435
+harry = 34
+sameer = 45
+_sameer = 34
+# @sameer = 56 # Invalid due to @ symbol
+# s@meer = Invalid due to @ symbol
+
+# Operators in Python
+
+# Arithmetic Operators
+x = 7
+y = 4
+z = x + y
+print(z)
+
+# Assignment Operators
+x = 4-2 # Assign 4-2 in x
+print(x)
+y = 6
+# y += 3 # Increment the value of y by 3 and then assign it to y
+y /= 3 # Decrement the value of y by 3 and then assign it to y
+print(y)
+
+# Comparision Operators
+d = 5==5
+print(d)
+
+# Logical Operators
+e = True or False
+
+# Truth table of 'or'
+print("True or False is " , True or False )
+print("False or False is " , False or False )
+print("True or True is ", True or True )
+print("False or True is ", False or True )
+
+# Truth table of 'and'
+print("True and False is " , True and False )
+print("False and False is " , False and False )
+print("True and True is ", True and True )
+print("False and True is ", False and True )
+
+print(not(False))
+print(not(True))
+
+# Type Function and Typecasting
+p = "31.2"
+q = float(p)  # Converts string/int into float
+t = type(q) 
+print(t)
+
+# Input Function
+# a = int(input ("Enter number 1: ")
+# b = int(input ("Enter number 2: "))
+# print("Number a is: ", a )
+# print("Number b is: ", b)
+# print("Sum is " , a + b)
+
+
