4a7933a619e84cefc9e9e08a165e7163c34d1151
radha-codes <prabhakarradha428@gmail.com>
2026-07-12 13:05:17 +0530

    Add files via upload

---
+++ ch2_practice_set.py
@@ -0,0 +1,28 @@
+# Problem 1 : Write a python program to add two numbers.
+a = 1
+b = 5
+print(a+b)
+
+# Problem 2 : Write a python program to find remainder when a number is divided by z
+x = 34 
+y = 5
+print("Remainder when x is divided by y is ", x % y )
+
+# Problem 3 : Check the type of variables assigned using input() function
+p = input ("Enter the value of p: ")
+print(type(p))
+
+# Problem 4 : Use comparison operator to find out whether a given variable 'm' is greater than 'n' or not. Take m=34 and n=80
+m = int(input("Enter number 1: "))
+n = int(input("Enter number 2: "))
+print("m is greater than n is ", m>n)
+
+# Problem 5 : Write a python program to find an average of two numbers entered by the user.
+m = int(input("Enter number 1: "))
+n = int(input("Enter number 2: "))
+print("The average of these two number is", (m+n)/2)
+
+# Problem 6 : Write a python program to calculate the square of a number entered by the user.
+a = int(input("Enter your number: "))
+print("The square of the number is " , a**2)
+# print ("The square of the number is", a^2) # Incorrect for finding square of a number in Python
