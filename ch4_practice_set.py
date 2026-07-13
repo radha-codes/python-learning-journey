ace4f695d1953ad11990332486d94a48a126bfc4
radha-codes <prabhakarradha428@gmail.com>
2026-07-13 16:27:25 +0530

    Add files via upload

---
+++ ch4_practice_set.py
@@ -0,0 +1,48 @@
+#  Problem 1 : Write a program to store seven markss in a list etered by the user.
+# fruit = []
+# f1 = input("Enter fruit name: ")
+# fruit.append(f1)
+# f2 = input("Enter fruit name: ")
+# fruit.append(f2)
+# f3 = input("Enter fruit name: ")
+# fruit.append(f3)
+# f4 = input("Enter fruit name: ")
+# fruit.append(f4)
+# f5 = input("Enter fruit name: ")
+# fruit.append(f5)
+# f6 = input("Enter fruit name: ")
+# fruit.append(f6)
+# f7 = input("Enter fruit name: ")
+# fruit.append(f7)
+#print(fruit)
+
+# Problem 2 : Write a program to accept marks of 6 students and display them in a sorted manner.
+# marks = []
+# f1 = int(input("Enter marks here: "))
+# marks.append(f1)
+# f2 = int(input("Enter marks here: "))
+# marks.append(f2)
+# f3 = int(input("Enter marks here: "))
+# marks.append(f3)
+# f4 = int(input("Enter marks here: "))
+# marks.append(f4)
+# f5 = int(input("Enter marks here: "))
+# marks.append(f5)
+# f6 = int(input("Enter marks here: "))
+# marks.append(f6)
+# marks.sort()
+# print(marks)
+
+# Problem 3 : Check that tupple type cannot be changed in python.
+# a = (34, 234, "Harry")
+# a[2] = "larry"
+
+# Problem 4 : Write a program to sum a list with 4 numbers.
+# l = [34, 56, 78, 98 ]
+# print(sum(l))
+
+# Problem 5 : Write a program to count the numbers of zeros in the following tupple.
+a = (7, 0, 8, 0, 0, 9)
+n = a.count(0)
+print(n)
+
