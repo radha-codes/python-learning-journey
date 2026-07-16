c2d89310ad7d4942293b79ab6d8b5dc54c7fbb4b
radha-codes <prabhakarradha428@gmail.com>
2026-07-16 18:14:40 +0530

    Add files via upload

---
+++ ch7_loops.py
@@ -0,0 +1,109 @@
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+
+# The same task can be done like this :
+# for i in range(1, 6):
+#     print(i)
+# for i in range(1, 1000):
+#     print(i)
+# for i in range (100, 999):
+#     print(i)
+
+# While loop
+
+# i = 1
+
+# while(i<51):
+#     print(i)
+#     i +=1 # i = i + 1
+# '''
+# Output:
+# 1
+# 2
+# 3
+# 4
+# 5
+# '''
+
+# List using while loop
+
+# l = [1, "Harry", False, "This", "Rohan", "Shubham", "Shubhi"]
+# i = 0
+# while(i<len(l)):
+#     print(l[i])
+#     i +=1
+
+# l = [1, "Harry", False, "This", "Rohan", "Shubham", "Shubhi"]
+# i = 4
+# while(i<len(l)):
+#     print(l[i])
+#     i +=1
+
+# For loops
+
+# for i in range (4):
+#     print(i)
+
+# l = [1, 4, 6, 234, 6, 764]
+# for i in l:
+#     print(i)
+
+# t = (6, 231, 75, 122)
+# for i in t:
+#     print(i)
+
+# l = [1, 7, 8]
+
+# for item in l:
+#     print(item)
+
+# else:
+#     print("done") # this is printed when the loop exhausts! 
+
+
+# For loop iterate
+
+
+# For Loop with Lists
+# l = [1, 7, 8, 234, 890, 764]
+# for i in l:
+#     print(i)
+
+# For Loop with Tuples
+# t = (3, 78, 90, 567, 890)
+# for i in t:
+#     print(i)
+
+# For Loop with Strings
+# s = "Harry"
+# for i in s:
+#     print(i)
+
+# For with else
+# l = [1, 7, 8]
+# for item in l:
+#     print(item)
+# else:
+#     print("done") # this is printed when the loop exhausts!
+
+# for i in range (100) :
+#     if (i == 34):
+#      break # Exit the loop right now
+#     print(i)
+
+# for i in range (100) :
+#     if (i == 34):
+#      continue # Skip this iteration
+#     print(i)
+
+# Pass
+# for i in range (645):
+#     pass
+
+# i = 0
+# while(i<45):
+#     print(i)
+#     i +=1
